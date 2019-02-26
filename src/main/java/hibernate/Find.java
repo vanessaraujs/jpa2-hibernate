81fa510393197bc940b336862393f683429ffba2
Vanessa Araujo <vanessaraujs@gmail.com>
2019-02-26 01:10:20 -0300

    Consultando Pessoa no banco de dados

---
+++ src/main/java/hibernate/Find.java
@@ -0,0 +1,25 @@
+package hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import hibernate.domains.Pessoa;
+
+public class Find {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-hibernate");
+		EntityManager em = emf.createEntityManager();
+
+		Pessoa pessoa = em.find(Pessoa.class, 5L);
+
+		if (pessoa != null) {
+			System.out.println("\nNome: " + pessoa.getNome() + "\n" + "Idade: " + pessoa.getIdade());
+		} else {
+			System.out.println("Pessoa não encontrada.");
+		}
+
+	}
+
+}
